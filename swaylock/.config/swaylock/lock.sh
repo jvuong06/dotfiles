e7e4d1d2bd3d57ec29daf6c0ac147e91d2ff0ece
jvuong06 <jackvuong06@gmail.com>
2025-12-15 01:39:25 -0500

    Rudimentary files

---
+++ swaylock/.config/swaylock/lock.sh
@@ -0,0 +1,22 @@
+image="$HOME/.swaylock/clouds_above_a_mountain.png"
+
+swaylock \
+    --daemonize \
+    --ignore-empty-password \
+    --indicator-caps-lock \
+    --indicator-radius 160 \
+    --indicator-thickness 20 \
+    --color 000000FF \
+    --inside-color 00000000 \
+    --inside-clear-color 00000000 \
+    --inside-ver-color 00000000 \
+    --inside-wrong-color 00000000 \
+    --line-color 00000000 \
+    --line-clear-color 00000000 \
+    --line-caps-lock-color 00000000 \
+    --line-ver-color 00000000 \
+    --line-wrong-color 00000000 \
+    --show-keyboard-layout \
+    --image "$image" \
+    --font "JetBrainsMono Nerd Font" \
+    --font-size 24 \
